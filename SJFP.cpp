aeab786cd28ef6d05c3aba88e1dc2926fd2be380
Dev-Nan <70539964+Dev-Nan@users.noreply.github.com>
2022-11-27 23:25:32 +0530

    Add files via upload

--- SJFP.cpp
+++ SJFP.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 // PID,name,type,Priority,AT,BT
 
+class SJFP{
+    public:
 struct Process
 {
     // populating from input
@@ -12,6 +14,9 @@ struct Process
     int priority;
     int arrival_time;
     int burst_time;
+    int burst_time1;
+    int burst_time2;
+    int IO_time;
     // derived variables
     int start_time;       // timestamp of first-time cpu allocation
     int completion_time;  // timestamp of termination
@@ -50,7 +55,7 @@ struct cmpId
 priority_queue<struct Process, vector<struct Process>, cmPtq> Cpt;
 priority_queue<struct Process, vector<struct Process>, cmPtq> IOpt;
 map<int, int> Arrival;
-map<int, pair<bool, int>> Vis;
+map<int, pair<bool, int> > Vis;
 
 void ProcADD(int &cur_time, vector<struct Process> &CPU_Proc)
 {
@@ -71,77 +76,64 @@ void ProcADD(int &cur_time, vector<struct Process> &CPU_Proc)
 
     return;
 }
-
-void IoADD(int &cur_time, vector<struct Process> &IO_Proc)
-{
-    if (IO_Proc.size() == 0)
-        return;
-    if (IOpt.empty() && cur_time < IO_Proc[0].arrival_time)
-        cur_time = IO_Proc[0].arrival_time;
-    int idx = 0, siz = IO_Proc.size();
-    while (idx < siz && cur_time >= IO_Proc[idx].arrival_time)
-    {
-        IOpt.push(*(IO_Proc.begin()));
-        IO_Proc.erase(IO_Proc.begin());
-        idx++;
+vector<string> split(string str,char delimiter){
+    int n = str.length();
+    vector<string> result;
+    string temp = "";
+    for(int i=0;i<n;i++){
+        char ch = str[i];
+        if(ch != delimiter){
+            temp.append(1,ch);
+        }
+        else{
+            result.push_back(temp);
+            temp = "";
+        }
     }
-
-    return;
+    if(temp != ""){
+        result.push_back(temp);
+    }
+    return result;
 }
 
-int main()
+void exec(vector<string> Vec)
 {
-    int n, pd, pr, art, bt;
-    bool ci;
-    string PID, Priority, AT, BT, Type, Name;
-    ifstream inp("ab.csv");
+    string PID, Priority, AT, BT1, BT2, IOt, Type, Name;
     struct Process obj;
     vector<struct Process> INP;
-    while (inp.peek() != EOF)
+    for (string proc : Vec)
     {
-        getline(inp, PID, ',');
-        getline(inp, Name, ',');
-        getline(inp, Type, ',');
-        getline(inp, Priority, ',');
-        getline(inp, AT, ',');
-        getline(inp, BT, '\n');
-        obj.PID = (stoi(PID));
-        obj.name = Name;
-        obj.cpu_bound = (stoi(Type));
-        obj.priority = (stoi(Priority));
-        obj.arrival_time = (stoi(AT));
-        obj.burst_time = (stoi(BT));
+        vector<string> splits = split(proc, ' ');
+        obj.PID = stoi(splits[0]);
+        obj.name = splits[1];
+        obj.priority = (stoi(splits[3]));
+        obj.arrival_time = (stoi(splits[4]));
+        obj.burst_time1 = (stoi(splits[5]));
+        obj.burst_time = (stoi(splits[5]));
+        obj.IO_time = (stoi(splits[6]));
+        obj.burst_time2 = (stoi(splits[7]));
         INP.push_back(obj);
     }
-    inp.close();
 
     // Sorting on the basis of Arrival Time
     sort(INP.begin(), INP.end(), cmpArv());
 
     vector<struct Process> CPU_Proc;
-    vector<struct Process> IO_Proc;
     vector<struct Process> CPU_Op;
-    vector<struct Process> IO_Op;
     vector<struct Process> Ans;
     int idx, cur_time = 0, entr;
     struct Process tp;
     for (idx = 0; idx < INP.size(); idx++)
     {
-        if (INP[idx].cpu_bound)
-            CPU_Proc.push_back(INP[idx]);
-        else
-        {
-            IO_Proc.push_back(INP[idx]);
-        }
+        CPU_Proc.push_back(INP[idx]);
         Arrival[INP[idx].arrival_time]++;
         Vis[INP[idx].PID] = make_pair(false, INP[idx].burst_time);
     }
-    
 
     ProcADD(cur_time, CPU_Proc);
 
     while (!Cpt.empty())
-    { 
+    {
         tp = Cpt.top();
         Cpt.pop();
         if (Vis[tp.PID].first == false)
@@ -173,58 +165,21 @@ int main()
 
         ProcADD(cur_time, CPU_Proc);
     }
-    cout << endl;
     // code for I/O Process
-
-    cur_time = 0;
-    IoADD(cur_time, IO_Proc);
-    while (!IOpt.empty())
-    {
-        tp = IOpt.top();
-        IOpt.pop();
-        if (Vis[tp.PID].first == false)
-        {
-            tp.start_time = cur_time;
-            Vis[tp.PID].first = true;
-        }
-        entr = 1;
-        while (tp.burst_time > 0 && (Arrival[cur_time] == 0) || entr)
-        {
-            cur_time++;
-            tp.burst_time--;
-            entr = 0;
-        }
-
-        if (tp.burst_time == 0)
-        {
-            tp.completion_time = cur_time;
-            tp.burst_time = Vis[tp.PID].second;
-            tp.turn_around_time = tp.completion_time - tp.arrival_time;
-            tp.response_time = tp.start_time - tp.arrival_time;
-            tp.waiting_time = tp.turn_around_time - tp.burst_time;
-            IO_Op.push_back(tp);
-        }
-        else
-        {
-            IOpt.push(tp);
-        }
-
-        IoADD(cur_time, CPU_Proc);
-    }
 
     for (idx = 0; idx < CPU_Op.size(); idx++)
         Ans.push_back(CPU_Op[idx]);
 
-    for (idx = 0; idx < IO_Op.size(); idx++)
-        Ans.push_back(IO_Op[idx]);
-
     sort(Ans.begin(), Ans.end(), cmpId());
-
+    string s;
+    ofstream wrt("SJFP_output.csv");
+    wrt << "id,name,priority,arrival,burst1,io,burst2,start,resp,compl,turn,wait\n";
     for (idx = 0; idx < Ans.size(); idx++)
     {
-        cout << Ans[idx].PID << " "; //<<Ans[idx].name<<" "<<Ans[idx].cpu_bound<<" "<<Ans[idx].priority<<" "<<Ans[idx].arrival_time<<" "<<Ans[idx].burst_time<<" ";
-        cout << Ans[idx].start_time << " " << Ans[idx].completion_time << " " << Ans[idx].waiting_time << " " << Ans[idx].turn_around_time << " " << Ans[idx].response_time << endl;
+        s = (to_string(Ans[idx].PID) + "," + Ans[idx].name + "," + to_string(Ans[idx].priority) + "," + to_string(Ans[idx].arrival_time) + "," + to_string(Ans[idx].burst_time1) + "," + to_string(Ans[idx].IO_time) + "," + to_string(Ans[idx].burst_time2) +
+             "," + to_string(Ans[idx].start_time) + "," + to_string(Ans[idx].response_time) + "," + to_string(Ans[idx].completion_time) + "," + to_string(Ans[idx].turn_around_time) + "," + to_string(Ans[idx].waiting_time) + "\n");
+        wrt << s;
     }
-
-    return 0;
+    wrt.close();
 }
+};
